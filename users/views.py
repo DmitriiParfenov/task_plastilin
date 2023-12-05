30b1460b5fc6388dbaee6cbb804e6cb6638aba9e
Dmitrii <dima12sik@yandex.ru>
2023-12-05 22:08:37 +0300

    added UserListAPIView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from users.models import User
+from users.serializers import UserListSerializer
+
 
 # Create your views here.
+class UserListAPIView(generics.ListAPIView):
+    """Для получения объектов модели User."""
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = (IsAuthenticated,)
+    
